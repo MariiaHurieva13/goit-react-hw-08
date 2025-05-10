22b263483fba945617f425da025231e1534f493c
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-10 20:14:25 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,35 +1,19 @@
-import css from "./Contact.module.css";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { FaUserAstronaut } from "react-icons/fa6";
-import { IoIosPhonePortrait } from "react-icons/io";
 import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contacts/operations";
+import css from "./Contact.module.css"
+import { deleteContactThunk } from "../../redux/contacts/operations";
 
-const Contact = ({ contact }) => {
-  const dispatch = useDispatch();
 
-  const handleDelete = () => dispatch(deleteContact(contact.id));
+           
+export default function Contact ({ data: { id, name, number} }) {
+    const dispatch = useDispatch()
+    
 
-  return (
-    <li>
-      <div className={css.contactCard}>
-        <div className={css.userInfo}>
-          <p className={css.userAbout}>
-            <FaUserAstronaut className={css.userIcon} />
-            {contact.name}
-          </p>
-          <p className={css.userAbout}>
-            <IoIosPhonePortrait className={css.userIcon} />
-            {contact.number}
-          </p>
-        </div>
-        <IconButton aria-label="delete" onClick={handleDelete}>
-          <DeleteIcon />
-        </IconButton>
-      </div>
-    </li>
-  );
-};
 
-export default Contact;
+ return (
+    <div className={css.contact}>
+        <p className={css.text}>{name}</p>
+        <p className={css.text}>{number}</p>
+        <button className={css.btn} onClick ={() => dispatch(deleteContactThunk(id))}>Delete</button>
+    </div>
+ )
+}
