9640850033f53b5fa667c53f3d825e4ccaab397b
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-10 20:36:14 +0200

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,46 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { register, logIn, logOut, refreshUser } from "./operations";
+import { logInThunk, logOutThunk, refreshThunk, registerThunk } from "./operation";
 
-const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    user: {
-      name: null,
-      email: null,
-    },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
+
+const initialState = {
+  user: {
+    name: null,
+    email: null,
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(register.fulfilled, (state, action) => {
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+}
+
+const slice = createSlice ({
+    name: 'auth',
+    initialState,
+    extraReducers: builder => {
+      builder.addCase(registerThunk.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(logIn.fulfilled, (state, action) => {
+      builder.addCase(logInThunk.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(logOut.fulfilled, (state) => {
-        state.user = { name: null, email: null };
-        state.token = null;
-        state.isLoggedIn = false;
-      })
-      .addCase(refreshUser.pending, (state) => {
-        state.isRefreshing = true;
-      })
-      .addCase(refreshUser.fulfilled, (state, action) => {
+      builder.addCase(logOutThunk.fulfilled, () => initialState
+      )
+      builder.addCase(refreshThunk.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
       })
-      .addCase(refreshUser.rejected, (state) => {
+      builder.addCase(refreshThunk.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      builder.addCase(refreshThunk.rejected, (state) => {
         state.isRefreshing = false;
-      });
-  },
-});
+      })
+    }
 
-export const authReducer = authSlice.reducer;
+})
+
+export const authReducer = slice.reducer;
