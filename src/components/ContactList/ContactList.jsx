09e196c9054c6fe9a5c613f41b61353d565a4fc1
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-10 20:17:43 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,32 @@
-import { useSelector } from "react-redux";
+import { SelectFilterContactsMemo, selectLoading, selectError  } from "../../redux/contacts/selectors";
 import Contact from "../Contact/Contact";
-import css from "./ContactList.module.css";
-import { selectFilteredContacts } from "../../redux/contacts/selectors";
+import css from "./ContactList.module.css"
+import { useSelector } from 'react-redux';
 
-const ContactList = () => {
-  const filteredContacts = useSelector(selectFilteredContacts);
 
-  return (
-    <ul className={css.contactsList}>
-      {filteredContacts.map((contact) => (
-        <Contact key={contact.id} contact={contact} />
-      ))}
-    </ul>
+
+ function ContactList () {
+
+  const filterContacts = useSelector(SelectFilterContactsMemo)
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+
+    return (<>
+   
+    <ul className={css.list}>
+        
+        {filterContacts.map(contact => (
+          <li key={contact.id}>
+            <Contact data={contact} />
+          </li>   ))}   
+     
+    </ul>    
+   {loading && !error && <h2>moments...</h2>}
+    
+    </>
+
   );
-};
+}
 
 export default ContactList;
