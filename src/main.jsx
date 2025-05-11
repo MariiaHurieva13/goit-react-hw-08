8b82d7569dca10ac6ac8f1c6b3907f59f6648090
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-11 17:55:22 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
-import { StrictMode } from "react";
-// import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import "modern-normalize";
-import "./index.css";
-import App from "./components/App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
+<BrowserRouter>
   <StrictMode>
-    <Provider store={store}>
+     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          {/* <HelmetProvider> */}
-          <App />
-          {/* </HelmetProvider> */}
-        </BrowserRouter>
+        <App />
       </PersistGate>
     </Provider>
   </StrictMode>
-);
+</BrowserRouter>
+)
