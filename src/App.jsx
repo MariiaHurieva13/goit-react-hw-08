b4c0ba002475913875968c5cf7e725058652b64e
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-11 19:57:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import Layout from './Layout.jsx'
-import PrivateRoute from './PrivateRoute.jsx'
-import RestrictedRoute from './RestrictedRoute.jsx'
+import Layout from './components/Layout.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import RestrictedRoute from './components/RestrictedRoute.jsx'
 
-import { refreshThunk } from '../redux/auth/operations.js'
-import { selectIsRefresh } from '../redux/auth/selectors.js'
+import { refreshThunk } from './redux/auth/operations.js'
+import { selectIsRefresh } from './redux/auth/selectors.js'
 
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const RegistrationPage = lazy(() => import('../pages/RegistrationPage/RegistrationPage'));
-const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
-const ContactsPage = lazy(() => import('../pages/ContactsPage/ContactsPage'));
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const RegistrationPage = lazy(() => import('./pages/RegistrationPage/RegistrationPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const ContactsPage = lazy(() => import('./pages/ContactsPage/ContactsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 function App() {
   const dispatch = useDispatch();
