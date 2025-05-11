75026d671a9a52407773beff61441b11fe3a7e11
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-11 17:49:33 +0200

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,19 +1,19 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { selectNameFilter } from "../filters/selectors";
+import { selectNameFilter } from "../filters/selectors"
+import {createSelector } from "@reduxjs/toolkit"
+
 
 export const selectContacts = (state) => state.contacts.items;
 export const selectLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
-export const selectFilteredContacts = createSelector(
-  [selectContacts, selectNameFilter],
-  (contacts, filter) =>
-    contacts.filter((contact) => {
-      return (
-        contact.name
-          .toLocaleLowerCase()
-          .includes(filter.trim().toLocaleLowerCase()) ||
-        contact.number.includes(filter.trim())
-      );
-    })
-);
+
+export const SelectFilterContactsMemo = createSelector([selectContacts, selectNameFilter], (contacts, filterName)=>{
+
+    console.log(filterName)
+ 
+    if (!contacts) {return []}
+    if (!filterName) {return contacts}
+   
+    const filterContacts = contacts.filter(ar=>ar.name.toLowerCase().includes(filterName.toLowerCase()));
+    return filterContacts;
+})
